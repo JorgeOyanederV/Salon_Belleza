05f564e280274d31c7e7c72f8592a8ebefafe380
JorgeOyanederV <jorge.oyaneder.velasquez@gmail.com>
2021-02-28 23:08:39 -0300

    Transicion de Secciones

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,42 @@
+let pagina = 1;
+//Iniciamos la aplicacion
 document.addEventListener('DOMContentLoaded', function() {
     iniciarApp();
 })
 function iniciarApp(){
     mostrarServicios();
+    // resalta la seccion actual
+    mostrarSeccion();
+    //oculta la seccion segun el tab en que se encuentra 
+    cambiarSeccion();
+}
+
+function mostrarSeccion(){
+    const seccionActual = document.querySelector(`#paso-${pagina}`);
+    seccionActual.classList.add('mostrar-seccion');
+    //resalta el tab actual
+    const tab = document.querySelector(`[data-paso="${pagina}"]`);
+    tab.classList.add('actual');
+}
+
+function cambiarSeccion(){
+    const enlaces = document.querySelectorAll('.tabs button');
+    enlaces.forEach(enlace => {
+        enlace.addEventListener('click', evento => {
+            evento.preventDefault();
+            pagina = parseInt(evento.target.dataset.paso);
+            // Eliminar primero la seccion anterior con mostrar seccion
+            document.querySelector('.mostrar-seccion').classList.remove('mostrar-seccion');
+            // Agrega mostrar seccion donde dimos click
+            const seccion = document.querySelector(`#paso-${pagina}`);
+            seccion.classList.add('mostrar-seccion');
+            // Eliminar la clase actual en el tab anterior
+            document.querySelector('.tabs .actual').classList.remove('actual');
+            // Agregar la clase actual en el tab actual 
+            const tab = document.querySelector(`[data-paso="${pagina}"]`);
+            tab.classList.add('actual');
+        })
+    })
 }
 async function mostrarServicios(){
     try {
@@ -52,5 +86,4 @@ function seleccionarServicio(evento){
     }else{
         elemento.classList.add('seleccionado');
     }
-    console.log(elemento.classList);
 }
